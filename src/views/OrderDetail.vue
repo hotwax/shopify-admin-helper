38d1566d8479044c3c5d6fff3908812ac1390d73
Disha Talreja <dishatalreja1202@gmail.com>
2022-08-04 19:00:02 +0530

    Renamed line item property from Pickup Store to Store Pickup(#2nvxwh1)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -129,11 +129,11 @@ export default defineComponent({
     },
     markBopisItem (item: any, event: any) {
       if(this.isBopisItem(item)){
-        item.properties = item.properties.filter((property: any) => !(property.name === '_pickupstore' || property.name === 'Pickup Store'))
+        item.properties = item.properties.filter((property: any) => !(property.name === '_pickupstore' || property.name === 'Store Pickup' || property.name === 'Pickup Store'))
       } else {
         const store = this.shopifyStores[0];
         const address = [store.storename, store.address1, store.city].filter((value: any) => value).join(", ");
-        item.properties.push({ name: '_pickupstore', value: store.storeCode }, { name: 'Pickup Store', value: address })
+        item.properties.push({ name: '_pickupstore', value: store.storeCode }, { name: 'Store Pickup', value: address })
       }
       this.store.dispatch('order/updateLineItems', this.order)
     },
